e9213cf8eacff82cc4ee0199f863071338fa0881
Andrew James <ajmacdonald@users.noreply.github.com>
2022-01-26 11:31:48 -0500

    build: set version in swing panel

--- src/org/voyanttools/server/ui/VoyantServerStart.java
+++ src/org/voyanttools/server/ui/VoyantServerStart.java
@@ -31,7 +31,7 @@ import org.voyanttools.server.ui.awt.VoyantServerTab;
  */
 public class VoyantServerStart extends Start {
 
-	public static final String VERSION = "2.2";
+	public static final String VERSION = "2.5.4";
 	
 	private JFrame frame;
 	
