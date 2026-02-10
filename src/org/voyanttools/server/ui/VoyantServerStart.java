5304ca44bd6c491dac2758366c2eb5b3cb351c71
Andrew MacDonald <ajmacdonald@users.noreply.github.com>
2026-02-10 10:31:44 -0500

    build: bump version

--- src/org/voyanttools/server/ui/VoyantServerStart.java
+++ src/org/voyanttools/server/ui/VoyantServerStart.java
@@ -31,7 +31,7 @@ import org.voyanttools.server.ui.awt.VoyantServerTab;
  */
 public class VoyantServerStart extends Start {
 
-	public static final String VERSION = "2.6.19";
+	public static final String VERSION = "2.6.20";
 	
 	private JFrame frame;
 	
